ef83f4a171272ae8a7ebf76619add077677bb346
Harel Moshe <harelmo@wix.com>
2019-05-01 14:55:53 +0300

    Split from isolated-runtime

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const path = require("path");
+const { IsolatedRuntime } = require("isolated-runtime");
+const npm = require("./lib/npm-modules");
+
+const compilerModulePath = require.resolve("vm2-babel-compiler");
+
+module.exports = ({ untrustedCodePath, edmPath, npmPath }) => {
+  const runtime = new IsolatedRuntime({
+    compilerModulePath,
+    resolverModulePath: path.resolve(__dirname, "./lib/resolver.js")
+  });
+
+  const app = express();
+  app.use("/:user/_functions/:name", async (req, res) => {
+    const { name, user } = req.params;
+    const method = req.method.toLowerCase();
+
+    const funcName = `${method}_${name}`;
+    const userRoot = path.join(untrustedCodePath, user);
+    let body = "";
+
+    const {
+      list: npmModules,
+      versionMap: npmVersionMap
+    } = await npm.listModules(npmPath);
+
+    req.on("data", chunk => {
+      body += chunk;
+    });
+
+    req.on("end", async () => {
+      const dependencies = await npm.listUserDependencies(userRoot);
+      const result = await runtime.run({
+        root: userRoot,
+        file: "backend/http-functions.js",
+        funcName,
+        args: {
+          resolverPath: path.resolve(
+            __dirname,
+            "./lib/http-function-args-resolver.js"
+          ),
+          original: [
+            req.originalUrl,
+            req.method,
+            body,
+            req.headers,
+            req.connection.remoteAddress
+          ]
+        },
+        external: {
+          modules: ["backend/*", "wix-http-functions", ...npmModules],
+          transitive: true
+        },
+        whitelistedPaths: [edmPath, npmPath],
+
+        resolverOptions: {
+          npmPath,
+          root: userRoot,
+          npmModules,
+          dependencies,
+          npmVersionMap
+        }
+      });
+
+      res.status(result.status);
+      Object.keys(result.headers).forEach(h => {
+        res.set(h, result.headers[h]);
+      });
+      res.json(result.body);
+    });
+  });
+
+  const server = app.listen(0);
+
+  return {
+    close: async () => {
+      await runtime.shutdown();
+      await server.close();
+    },
+    port: server.address().port
+  };
+};
---
+++ test/app.spec.js
@@ -0,0 +1,184 @@
+const path = require("path");
+const { Context, randoms } = require("isolated-runtime-test-commons");
+const axios = require("axios");
+const listen = require("..");
+
+describe("Isolated Runtime Server", () => {
+  let context;
+  let server;
+  let client;
+  let root;
+  let npmModulesContext;
+  let npmPath;
+
+  beforeEach(() => {
+    root = randoms.folder();
+    npmPath = path.join(__dirname, randoms.folder());
+    server = listen({
+      untrustedCodePath: __dirname,
+      edmPath: path.dirname(
+        path.dirname(require.resolve("wix-http-functions"))
+      ),
+      npmPath
+    });
+    client = axios.create({ baseURL: `http://localhost:${server.port}` });
+  });
+
+  afterEach(() => server.close());
+
+  describe("With user simple http function", () => {
+    beforeEach(async () => {
+      context = await new Context({
+        basePath: __dirname,
+        root
+      })
+        .withFile(
+          "backend/http-functions.js",
+          `
+            import { ok } from 'wix-http-functions';
+
+            const response = (todos) => ({
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: {
+                todos
+              }
+            })
+
+            export function get_todos(request) {
+              return ok(response(['todo1', 'todo2']));
+            }
+          `
+        )
+        .build();
+    });
+
+    afterEach(() => context.destroy());
+
+    test("Runs sandboxed code", async () => {
+      const response = await client.get(`${root}/_functions/todos/`);
+
+      expect(response.status).toBe(200);
+      expect(response.data).toEqual({
+        todos: ["todo1", "todo2"]
+      });
+    });
+  });
+
+  describe("With http-functions using npm modules", () => {
+    beforeEach(async () => {
+      npmModulesContext = await new Context({
+        basePath: npmPath,
+        root: "" // todo should have nested root with current
+      })
+        .withFile(
+          "map.json",
+          JSON.stringify({
+            "sample-module@0.0.1": "WC_BEGIN/sample-module/0.0.1/WC_END"
+          })
+        )
+        .withFile(
+          "WC_BEGIN/sample-module/0.0.1/WC_END/sample-module/index.js",
+          "module.exports = 'my-todo'"
+        )
+        .build();
+
+      context = await new Context({
+        basePath: __dirname,
+        root
+      })
+        .withFile(
+          "backend/wix-code-package-lock.json",
+          JSON.stringify({
+            dependencies: {
+              "sample-module": "0.0.1"
+            }
+          })
+        )
+        .withFile(
+          "backend/http-functions.js",
+          `
+              import { ok } from 'wix-http-functions';
+              import todoTitle from 'sample-module';
+
+              const response = (todos) => ({
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: {
+                  todos
+                }
+              })
+
+              export function get_todos_with_npm(request) {
+                return ok(response([todoTitle]));
+              }
+            `
+        )
+        .build();
+    });
+
+    afterEach(() =>
+      Promise.all([context.destroy(), npmModulesContext.destroy()])
+    );
+
+    test("runs sandboxed code using npm modules", async () => {
+      const response = await client.get(`${root}/_functions/todos_with_npm/`);
+
+      expect(response.status).toBe(200);
+      expect(response.data).toEqual({
+        todos: ["my-todo"]
+      });
+    });
+  });
+
+  describe("With user code requiring absolute user-modules", () => {
+    beforeEach(async () => {
+      context = await new Context({ basePath: __dirname, root })
+        .withFile(
+          "backend/http-functions.js",
+          `
+          import { ok } from 'wix-http-functions';
+          import { multiply } from 'backend/multiply.js'
+
+          export function get_multiply(request) {
+            const { a, b } = request.query
+
+            const response = {
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: {
+                result: multiply(a, b)
+              }
+            }
+
+            return ok(response);
+          }
+        `
+        )
+        .withFile(
+          "backend/multiply.js",
+          `
+          function multiply(a, b) {
+            return a * b
+          }
+
+          module.exports = {
+            multiply
+          }`
+        )
+        .build();
+    });
+
+    afterEach(() => context.destroy());
+
+    test("runs the user method with the requires and returns the result", async () => {
+      const response = await client.get(`${root}/_functions/multiply?a=4&b=6`);
+
+      expect(response.status).toBe(200);
+      expect(response.data).toEqual({ result: 24 });
+    });
+  });
+});
